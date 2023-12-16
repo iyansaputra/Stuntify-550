1d8dc8c0991ba67f98b8d69189bb5cd0c35822d0
YannIyann <120540178+iyansaputra@users.noreply.github.com>
2023-12-16 18:06:02 +0700

    Update database.js

--- Cloud Computing/api_stuntify/src/config/database.js
+++ Cloud Computing/api_stuntify/src/config/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const dbPool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'Pohonmangga90',
-    database: process.env.DB_DATABASE || 'stuntify'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE'
 });
 
 module.exports = dbPool.promise();
