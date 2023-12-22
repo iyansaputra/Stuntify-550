4ff997e1201322560aab6458dfb765348d68cb78
Tempoles <11woww90@gmail.com>
2023-12-22 13:14:04 +0700

    salah

--- Cloud Computing/stuntify_user_input/src/middleware/logs.js
+++
@@ -1,7 +0,0 @@
-// contoh middleware (untuk memantau server)
-const logRequest = (req, res, next) => {
-    console.log('API Running... in path', req.path);
-    next();
-}
-
-module.exports = logRequest;
